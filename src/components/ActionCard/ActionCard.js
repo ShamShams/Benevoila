279d681e7ca7fd3c36d066877ae3f9e2a0657338
sremtoula@simplon.co <sremtoula@simplon.co>
2018-08-09 15:02:44 +0200

    Fetch data with axios and parse date and time with moment

--- src/components/ActionCard/ActionCard.js
+++ src/components/ActionCard/ActionCard.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import moment from 'moment';
+
 import {
     ExpansionPanel,
     ExpansionPanelDetails,
@@ -14,52 +16,84 @@ import ContainedButton from '../ContainedButton';
 import './ActionCard.scss';
 
 const styles = () => ({
-    summary: { margin: '20px 0'},
+    summary: { margin: '20px 0' },
     details: { padding: '20px 24px' }
 });
 
-const ActionCard = ({ classes }) => (
-    <div className="action-card">
-        <ExpansionPanel>
-            <ExpansionPanelSummary
-                expandIcon={<ExpandMoreIcon />}
-                classes={{ content: classes.summary }}
-            >
-                <div className="action-card-summary">
-                    <div className="action-card-infos">
-                        <p className="action-card-date">12/06/2018</p>
-                        <p className="action-card-title">Maraude sociale</p>
-                        <p className="action-card-time-place">Paris 13  -  19h - 21h</p>
+const ActionCard = ({ classes, action }) => {
+    const {
+        action_type_id,
+        start_date,
+        end_date,
+        address,
+        zipcode,
+        city,
+        need
+    } = action;
+    return (
+        <div className="action-card">
+            <ExpansionPanel>
+                <ExpansionPanelSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    classes={{ content: classes.summary }}
+                >
+                    <div className="action-card-summary">
+                        <div className="action-card-infos">
+                            <p className="action-card-date">
+                                {moment(start_date).format('DD/MM/YYYY')}
+                            </p>
+                            <p className="action-card-title">
+                                {action_type_id}
+                            </p>
+                            <p className="action-card-time-place">
+                                {city} - {moment(start_date).format('H:mm')} -{' '}
+                                {moment(end_date).format('H:mm')}
+                            </p>
+                        </div>
+                        <div className="action-card-vlt-infos">
+                            <p>
+                                Inscrits :
+                                <span className="action-card-vlt-reg">0</span> -
+                                Manquants :
+                                <span className="action-card-vlt-needed">
+                                    {need}
+                                </span>
+                            </p>
+                        </div>
+                        <ContainedButton color="blueButton">
+                            Je m'inscris
+                        </ContainedButton>
                     </div>
-                    <div className="action-card-vlt-infos">
-                        <p>
-                            Inscrits : <span className="action-card-vlt-reg">11</span> - Manquants : <span className="action-card-vlt-needed">4</span>
-                        </p>
+                </ExpansionPanelSummary>
+                <Divider inset={true} />
+                <ExpansionPanelDetails classes={{ root: classes.details }}>
+                    <div className="action-card-details">
+                        <div className="action-card-description">
+                            <h4>Description</h4>
+                            <p>
+                                Les maraudes consistent à aller à la rencontre
+                                des personnes précaires et sans abri et leur
+                                proposer de l'aide
+                            </p>
+                        </div>
+                        {/*<div className="action-card-referent">
+                            <h4>Référent</h4>
+                            <p>
+                                Farid Hassan <br /> 06 12 76 54 98 <br />{' '}
+                                farid.hassan@asso.fr
+                            </p>
+                        </div>*/}
+                        <div className="action-card-address">
+                            <h4>Adresse</h4>
+                            <p>
+                                {address} <br /> {zipcode} {city}
+                            </p>
+                        </div>
                     </div>
-                    <ContainedButton color="blueButton">
-                        Je m'inscris
-                    </ContainedButton>
-                </div>
-            </ExpansionPanelSummary>
-            <Divider inset={true} />
-            <ExpansionPanelDetails classes={{ root: classes.details }}>
-                <div className="action-card-details">
-                    <div className="action-card-description">
-                        <h4>Description</h4>
-                        <p>Les maraudes consistent à aller à la rencontre des personnes précaires et sans abri et leur proposer de l'aide</p>
-                    </div>
-                    <div className="action-card-referent">
-                        <h4>Référent</h4>
-                        <p>Farid Hassan <br/> 06 12 76 54 98 <br/> farid.hassan@asso.fr</p>
-                    </div>
-                    <div className="action-card-address">
-                        <h4>Adresse</h4>
-                        <p>3 rue Maryse Bastié <br/> 75013 Paris</p>
-                    </div>
-                </div>
-            </ExpansionPanelDetails>
-        </ExpansionPanel>
-    </div>
-);
+                </ExpansionPanelDetails>
+            </ExpansionPanel>
+        </div>
+    );
+};
 
 export default withStyles(styles)(ActionCard);
--- src/components/ActionsList/ActionsList.js
+++ src/components/ActionsList/ActionsList.js
@@ -2,14 +2,44 @@ import React, { Component } from 'react';
 
 import ActionCard from '../ActionCard';
 
+import axios from 'axios';
+
 import './ActionsList.scss';
 
 class ActionsList extends Component {
+    state = {
+        actions: [],
+        isLoading: true,
+        errors: null
+    };
+
+    async getActions() {
+        const response = await axios.get('http://localhost:3000/actions');
+        try {
+            this.setState({
+                actions: response.data,
+                isLoading: false
+            });
+        } catch (error) {
+            this.setState({ error, isLoading: false });
+        }
+    }
+
+    componentDidMount() {
+        this.getActions();
+    }
+
     render() {
+        const { actions, isLoading } = this.state;
         return (
             <div className="action-list">
-                <ActionCard />
-                <ActionCard />
+                {!isLoading ? (
+                    actions.map(action => (
+                        <ActionCard key={action.action_id} action={action} />
+                    ))
+                ) : (
+                    <p>Chargement...</p>
+                )}
             </div>
         );
     }
