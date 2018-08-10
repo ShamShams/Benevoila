ecc292d020f9684053b9fcd97881233492eee37e
sremtoula@simplon.co <sremtoula@simplon.co>
2018-08-10 11:08:27 +0200

    Remove filter from Main

--- src/components/ActionsList/ActionsList.js
+++ src/components/ActionsList/ActionsList.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 
-import { FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
-
-// import Filter from '../Filter';
 import ActionCard from '../ActionCard';
 import Loader from '../Loader';
 
@@ -13,13 +10,8 @@ import './ActionsList.scss';
 class ActionsList extends Component {
     state = {
         actions: [],
-        actionType: '',
         isLoading: true,
         error: null
-    };
-
-    handleChange = event => {
-        this.setState({ [event.target.name]: event.target.value });
     };
 
     async getActions() {
@@ -41,50 +33,23 @@ class ActionsList extends Component {
     }
 
     render() {
-        const { actions, actionType, isLoading, error } = this.state;
+        const { actions, isLoading, error } = this.state;
         return (
             <div className={isLoading ? 'loader' : 'action-list'}>
                 {error ? <p>{error.message}</p> : null}
                 {!isLoading ? (
-                    <div>
-                        <form autoComplete="off">
-                            <FormControl>
-                                <InputLabel htmlFor="actionType">
-                                    Type d'action
-                                </InputLabel>
-                                <Select
-                                    value={this.state.actionType}
-                                    onChange={this.handleChange}
-                                    inputProps={{
-                                        name: 'actionType',
-                                        id: 'actionType'
-                                    }}
-                                >
-                                    <MenuItem value="">
-                                        <em>None</em>
-                                    </MenuItem>
-                                    {actions.map(action => (
-                                        <MenuItem value={action.name}>
-                                            {action.name}
-                                        </MenuItem>
-                                    ))}
-                                </Select>
-                            </FormControl>
-                        </form>
-                        {actions
-                            .sort((a, b) => {
-                                return (
-                                    new Date(a.start_date) -
-                                    new Date(b.start_date)
-                                );
-                            })
-                            .map(action => (
-                                <ActionCard
-                                    key={action.action_id}
-                                    action={action}
-                                />
-                            ))}
-                    </div>
+                    actions
+                        .sort((a, b) => {
+                            return (
+                                new Date(a.start_date) - new Date(b.start_date)
+                            );
+                        })
+                        .map(action => (
+                            <ActionCard
+                                key={action.action_id}
+                                action={action}
+                            />
+                        ))
                 ) : (
                     <Loader />
                 )}
