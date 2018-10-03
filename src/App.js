11b7e6cacd2d9756038d6bef89afa5b7fbfaca11
sremtoula@simplon.co <sremtoula@simplon.co>
2018-10-03 18:20:56 +0200

    Add Mui theme

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React, { Component } from 'react';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import theme from './theme';
 import Main from './components/Main';
 import SideBar from './components/SideBar';
+
 
 class App extends Component {
     render() {
         return (
-            <Router>
-                <div>
-                    <SideBar />
-                    <Switch>
-                        <Route
-                            exact path="/"
-                            render={() => <Main page="allActions" />}
-                        />
-                        <Route
-                            path="/useractions"
-                            render={() => <Main page="useractions" />}
-                        />
-                        <Route path="/profile" component={Main} />
-                    </Switch>
-                </div>
-            </Router>
+            <MuiThemeProvider theme={theme}>
+                <Router>
+                    <div>
+                        <SideBar />
+                        <Switch>
+                            <Route
+                                exact path="/"
+                                render={() => <Main page="allActions" />}
+                            />
+                            <Route
+                                path="/useractions"
+                                render={() => <Main page="useractions" />}
+                            />
+                            <Route path="/profile" component={Main} />
+                        </Switch>
+                    </div>
+                </Router>
+            </MuiThemeProvider>
         );
     }
 }
---
+++ src/theme.js
@@ -0,0 +1,45 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+import cyan from '@material-ui/core/colors/cyan';
+import red from '@material-ui/core/colors/red';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            light: cyan[800], // #00838F
+            main: cyan[600], // #00acc1
+            dark: cyan[400], // #26C6DA
+        },
+        secondary: {
+            light: red[400], // #EF9A9A
+            main: '#ee6e73',
+            dark: '#cf6367'
+        }
+    },
+    overrides: {
+        MuiButton: {
+            root: {
+                '&:hover': {
+                    backgroundColor: 'rgba(0, 0, 0, 0.08)'
+                }
+            },
+            text: {
+                color: '#666',
+                fontWeight: 'bold',
+                letterSpacing: '0.5px'
+            },
+            textPrimary: {
+                '&:hover': {
+                    backgroundColor: 'rgba(0, 0, 0, 0.08)'
+                }
+            },
+            textSecondary: {
+                '&:hover': {
+                    backgroundColor: 'rgba(0, 0, 0, 0.08)'
+                }
+            }
+        }
+    }
+});
+
+export default theme;
